8a790aff4a3a2d8d2e2bdd25585872f15bc63c86
gabor <tarrga88@gmail.com>
2022-05-30 13:07:44 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className='App'>
       {authIsReady && (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           {user && <Sidebar />}
           <div className='container'>
             <Navbar />
